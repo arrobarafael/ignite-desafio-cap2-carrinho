91514bf6f9cf9ade4de6e303fa59e1306f763e16
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-05-17 23:59:32 -0300

    Feita listagem de itens na tela de carrinho e função de remoção de item

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -75,9 +75,12 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = cart.filter((item) => item.id !== productId);
+      setCart(updatedCart);
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart));
     } catch {
       // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
