b456188a1aaf2fa064179ceeb7dbb14dff914c31
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-05-17 21:46:41 -0300

    Listando produtos dinamicamente

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -33,6 +33,7 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
   });
 
   const addProduct = async (productId: number) => {
+    console.log('adicionando', productId);
     try {
       // TODO
     } catch {
