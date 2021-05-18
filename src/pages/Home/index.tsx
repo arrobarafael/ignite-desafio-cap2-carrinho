c8dc6291d18cec95fbd7619612e0060fcbe6415e
Rafael Pelegrini <arrobarafael@hotmail.com>
2021-05-17 23:28:03 -0300

    Adicionada exibição de quantos itens de cada estão no carrinho

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,9 +25,16 @@ const Home = (): JSX.Element => {
   const [products, setProducts] = useState<ProductFormatted[]>([]);
   const { addProduct, cart } = useCart();
 
-  // const cartItemsAmount = cart.reduce((sumAmount, product) => {
-  //   // TODO
-  // }, {} as CartItemsAmount)
+  const cartItemsAmount = cart.reduce((sumAmount, product) => {
+    return {
+      ...sumAmount,
+      [product.id]: product.amount,
+    };
+    // return sumAmount;
+  }, {} as CartItemsAmount);
+
+  console.log('cartItemsAmount');
+  console.log(cartItemsAmount);
 
   useEffect(() => {
     async function loadProducts() {
@@ -57,7 +64,7 @@ const Home = (): JSX.Element => {
           >
             <div data-testid="cart-product-quantity">
               <MdAddShoppingCart size={16} color="#FFF" />
-              {/* {cartItemsAmount[product.id] || 0} */} 2
+              {cartItemsAmount[product.id] || 0}
             </div>
 
             <span>ADICIONAR AO CARRINHO</span>
